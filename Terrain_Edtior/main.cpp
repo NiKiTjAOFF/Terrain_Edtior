4753869b318d2d8f5690de3d9ec4ca0008d6f129
NiKiTjA_OFF <mr.zeydenz@mail.ru>
2025-05-19 23:52:21 +0300

    Added basic vertex and fragment shaders. The program can load, compile and link shaders in shader program. The program can represent shape data with different attributes using VAO. FINALLY DREW A FRICKING TRIANGLE, HELL YEAH!

--- Terrain_Edtior/main.cpp
+++ Terrain_Edtior/main.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <fstream>
+#include <sstream>
+#include <streambuf>
+#include <string>
 
-int WIDTH = 640, HEIGHT = 480;
-int OPENGL_VERSION_MAJOR = 3, OPENGL_VERSION_MINOR = 4;
+const int WIDTH = 640, HEIGHT = 480;
+const int OPENGL_VERSION_MAJOR = 3, OPENGL_VERSION_MINOR = 4;
+const int ERROR_SIZE = 512;
+glm::vec4 peachColor = { 1.0f, 0.898f, 0.706f, 1.0f };//glm test
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow* window);
+std::string loadShaderSrc(const char* filename);
 
 int main()
 {
+	int success;
+	char infoLog[ERROR_SIZE];
+
+	/*---------------------------------Initialization---------------------------------*/
 	//Initializes OpenGL
 	glfwInit();
 
@@ -27,7 +41,7 @@ int main()
 		return -1;
 	}
 
-	//Use newly created window
+	//Use newly created window to draw frames in it
 	glfwMakeContextCurrent(window);
 	
 	//Loads all needed functions for OpenGL
@@ -41,23 +55,110 @@ int main()
 	glViewport(0, 0, WIDTH, HEIGHT);
 	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
+
+	/*---------------------------------Setting Up Shaders---------------------------------*/
 	
+	//Vertex Shader Compilation
+	unsigned int vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	std::string vertexShaderSource = loadShaderSrc("./vertex_shader.glsl");
+	const GLchar* vertexShader = vertexShaderSource.c_str();
+	glShaderSource(vertexShaderId, 1, &vertexShader, NULL);
+	glCompileShader(vertexShaderId);
+
+	//Check for errors while compilating Vertex Shader
+	glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShaderId, ERROR_SIZE, NULL, infoLog);
+		std::cout << "Vertex Shader compilated with error: " << infoLog << '\n';
+		return -1;
+	}
+
+	//Fragment Shader Compilation
+	unsigned int fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+	std::string fragmentShaderSource = loadShaderSrc("./fragment_shader.glsl").c_str();
+	const GLchar* fragmentShader = fragmentShaderSource.c_str();
+	glShaderSource(fragmentShaderId, 1, &fragmentShader, NULL);
+	glCompileShader(fragmentShaderId);
+
+	//Check for errors while compilating Fragment Shader
+	glGetShaderiv(fragmentShaderId, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShaderId, ERROR_SIZE, NULL, infoLog);
+		std::cout << "Fragment Shader compilated with error: " << infoLog << '\n';
+		return -1;
+	}
+
+	//Create Shader Program
+	unsigned int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShaderId);
+	glAttachShader(shaderProgram, fragmentShaderId);
+	glLinkProgram(shaderProgram);
+
+	//Check for errors while linking shaders in program
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, ERROR_SIZE, NULL, infoLog);
+		std::cout << "Shader linked in program with error: " << infoLog << '\n';
+		return -1;
+	}
+
+	//Deleting compilated shader because we have a program
+	glDeleteShader(vertexShaderId);
+	glDeleteShader(fragmentShaderId);
+
+
+	/*---------------------------------Preparing Data to Render---------------------------------*/
+
+	//Vertex array
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f,
+		0.5f, -0.5f, 0.05f
+	};
+
+	//VAO, VBO creation
+	unsigned int VAO, VBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	//Bind VAO, VBO, to set them as active, and provide vertices to VBO
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Add VBO (pure vertices) as the first attribute to VAO
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+
+
+	/*---------------------------------Render, Calculate, Process, Repeat---------------------------------*/
 
 	//Run window until closed for a reason
 	while (!glfwWindowShouldClose(window))
 	{
+		//Process user inputs
 		processInput(window);
 
 		//Set the flush color and flush back buffer
-		glClearColor(1.0f, 0.898f, 0.706f, 1.0f);
+		glClearColor(peachColor.x, peachColor.y, peachColor.z, peachColor.a);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//Set current frame from other buffer and process any events related to a window
+		//Draw Shapes from VAO with needed attributes, using needed program, starting from 0, 3 numbers per vertex
+		glBindVertexArray(VAO);
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//Set current frame from back buffer and process any events related to a window
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-		
 	}
 
+
+	/*---------------------------------End Program Correctly---------------------------------*/
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
@@ -74,4 +175,28 @@ void processInput(GLFWwindow* window)
 	{
 		glfwSetWindowShouldClose(window, true);
 	}
+}
+
+std::string loadShaderSrc(const char * filename)
+{
+	std::ifstream file;
+	std::stringstream buf;
+
+	std::string ret = "";
+	file.open(filename);
+
+	if (file.is_open())
+	{
+		buf << file.rdbuf();
+		ret = buf.str();
+	}
+	else
+	{
+		std::cout << "Couldn't open " << filename << "\n";
+		 
+	}
+
+	file.close();
+	
+	return ret;
 }
